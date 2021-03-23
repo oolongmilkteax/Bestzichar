f5493c2f837c4a08b3c45de26e74dab99a8918eb
oolongmilkteax <rachelllhooo@gmail.com>
2021-03-23 17:45:11 +0800

    Finish product

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>~ Best Zi Char ~</h1>
+    <navbar></navbar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+h1{
+  background-color: pink;
+  height: 50px;
+  text-align: center;
+  font-size: 40px;
+  padding-top:10px;
+  font-family: fantasy;
+}
+</style>
